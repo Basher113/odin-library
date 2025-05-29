e1d0cc5d23d0ee9a0f62c34b4d0923371e79c8e6
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-05-30 06:34:57 +0800

    Add delete feature

--- script.js
+++ script.js
@@ -20,8 +20,6 @@ function addBookToLibrary(title, author, pages, haveRead) {
 addBookToLibrary("Harry Potter", "J.K Rowling", 450, "off");
 addBookToLibrary("The subtle art of not giving a f*ck", "Mark Manson", 244, "on")
 
-
-
 const createBookCardElement = function (book) {
   // Create elements for the book card
   const bookCardElement = document.createElement("div");
@@ -29,10 +27,9 @@ const createBookCardElement = function (book) {
   const bookInfoElement = document.createElement("div");
   const bookTitleElement = document.createElement("h3");
   const bookAuthorElement = document.createElement("div");
-  const bookAuthorSpanElement = document.createElement("span");
-  const bookPagesSpanElement = document.createElement("span");
   const bookPagesElement = document.createElement("div");
   const bookHaveReadElement = document.createElement("div");
+  const bookDeleteButtonElement = document.createElement("button");
 
   // Add class to new elements
   bookCardElement.classList.add("book-card");
@@ -42,6 +39,7 @@ const createBookCardElement = function (book) {
   bookAuthorElement.classList.add("author");
   bookPagesElement.classList.add("pages");
   bookHaveReadElement.classList.add("read");
+  bookDeleteButtonElement.classList.add("delete-button");
 
   // Add content to new elements
   bookImageElement.textContent = "Book Image";
@@ -49,25 +47,22 @@ const createBookCardElement = function (book) {
   bookAuthorElement.innerHTML = "<span>Author: </span>" + book.author;
   bookPagesElement.innerHTML = "<span>Pages: </span>" + book.pages;
   bookHaveReadElement.textContent = book.haveRead;
+  bookDeleteButtonElement.textContent = "Delete"
   
   // Appending elements
   bookInfoElement.appendChild(bookTitleElement);
   bookInfoElement.appendChild(bookAuthorElement);
   bookInfoElement.appendChild(bookPagesElement);
   bookInfoElement.appendChild(bookHaveReadElement);
+  bookInfoElement.appendChild(bookDeleteButtonElement);
 
   bookCardElement.appendChild(bookImageElement);
   bookCardElement.appendChild(bookInfoElement);
 
-
+    
+  bookDeleteButtonElement.setAttribute("data-id", book.id);
+  bookCardElement.setAttribute("id", book.id)
   return bookCardElement;
-}
-
-const renderBooks = function() {
-  myLibrary.forEach((book) => {
-    const bookCardElement = createBookCardElement(book);
-    booksElement.appendChild(bookCardElement);
-  })
 }
 
 formElement.addEventListener("submit", (e) => {
@@ -77,12 +72,39 @@ formElement.addEventListener("submit", (e) => {
   const newBookAuthor = formData.get("author");
   const newBookPages = formData.get("pages");
   const newBookHaveRead = formData.get("read");
-  console.log(newBookTitle, newBookAuthor, newBookPages, newBookHaveRead);
   const newBook = addBookToLibrary(newBookTitle, newBookAuthor, newBookPages, newBookHaveRead);
   const bookCardElement = createBookCardElement(newBook);
+  
   booksElement.appendChild(bookCardElement);
+  
+  // call so we can delete the new element if delete button is clicked
+  canDeleteElement()
 })
 
+const renderBooks = function() {
+  myLibrary.forEach((book) => {
+    const bookCardElement = createBookCardElement(book);
+    booksElement.appendChild(bookCardElement);
+  })
+}
+
+const canDeleteElement = function () {
+  const bookDeleteButtonElements = document.querySelectorAll(".delete-button");
+  bookDeleteButtonElements.forEach((deleteButton, index) => {
+  deleteButton.addEventListener("click", () => {
+    // remove from myLibrary
+    myLibrary.splice(index, 1);
+    const bookId = deleteButton.dataset.id;
+    console.log(bookId);
+    const bookCardElement = document.getElementById(bookId);
+    console.log(bookCardElement)
+    bookCardElement.remove();
+    })
+  })
+}
 
 
-renderBooks()
+
+renderBooks();
+canDeleteElement();
+
